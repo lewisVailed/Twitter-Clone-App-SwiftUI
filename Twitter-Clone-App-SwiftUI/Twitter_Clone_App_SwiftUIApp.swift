5828c8eb02edb2290311ec4bda34bc6265aff0eb
Ayberk Bilgiç <lewidvailed@outlook.com>
2022-11-15 14:31:39 +0300

    Initial Commit

---
+++ Twitter-Clone-App-SwiftUI/Twitter_Clone_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_Clone_App_SwiftUIApp.swift
+//  Twitter-Clone-App-SwiftUI
+//
+//  Created by Ayberk Bilgiç on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_Clone_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
